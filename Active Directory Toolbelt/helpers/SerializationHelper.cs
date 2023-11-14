97bcd45b54adc69d0d83630baf8bd24223e8703a
Dean Reid <deannreid@hotmail.co.uk>
2023-11-14 20:09:49 +0000

    Initial Code for Serialization

---
+++ Active Directory Toolbelt/helpers/SerializationHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Active Directory Toolbelt 
+ * Developed by @ Dean Reid
+ *
+ * Class Name: LocationHandler
+ *
+ * Class Information: helper class providing serialization/deserializing objects
+ *
+ * Class handles the location of the user. completed security checks and checks if user is allowed to access this program.
+ *
+ * 
+ *
+ * Program Version: 1.0
+ * Code Version: 1.0
+ *
+ * Updates:
+ * 14/11/2023 - Initial Code Development
+ *
+ */
+namespace Active_Directory_Toolbelt.helpers
+{
+    public static class SerializationHelper
+    {
+       
+    }
+}
