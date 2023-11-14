d719525f3157804f5c15c831f031ad3a38a9b1ac
Dean Reid <deannreid@hotmail.co.uk>
2023-11-14 20:50:58 +0000

    Initial Code for Authentication and Access Management

---
+++ Active Directory Toolbelt/handlers/AccessManagement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Active Directory Toolbelt 
+ * Developed by @ Dean Reid
+ *
+ * Class Name: AccessManagement
+ *
+ * Class Information:
+ *
+ * Class handles privilaged access management for the program
+ *
+ * Program Version: 1.0
+ * Code Version: 1.0
+ *
+ * Updates:
+ * 14/11/2023 - Initial Code Development
+ *
+ */
+namespace Active_Directory_Toolbelt.handlers
+{
+    internal class AccessManagement
+    {
+    }
+}
---
+++ Active Directory Toolbelt/helpers/AuthenticationHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Active Directory Toolbelt 
+ * Developed by @ Dean Reid
+ *
+ * Class Name: AuthenticationHelper
+ *
+ * Class Information:
+ *
+ * Class handles Static helper class providing tools for authenticating users
+ *
+ * Program Version: 1.0
+ * Code Version: 1.0
+ *
+ * Updates:
+ * 14/11/2023 - Initial Code Development
+ *
+ */
+namespace Active_Directory_Toolbelt.helpers
+{
+    public static class AuthenticationHelper
+    {
+        // create a static helper class to handle authentication with Active Directory
+
+        // setup openID connect authentication
+        // setup JWT authentication
+
+        // setup SAML authentication
+
+        // setup OAuth2 authentication
+
+        // setup WS-Federation authentication
+
+        // setup cookie authentication
+
+        // setup token authentication
+
+        // setup certificate authentication
+
+        // setup Azure AD authentication  
+
+
+
+
+
+    }
+}
